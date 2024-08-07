83d5c1081dc695f22125b9e106788cb146b9bf75
samueljsong <semuall0108@gmail.com>
2024-08-07 16:32:04 -0700

    created the basic layout using best practices for backend

--- src/controllers/jobController.js
+++ src/controllers/jobController.js
@@ -1,25 +1,32 @@
 /**
- * This Module is used for controlling the logic
- * for the functions regarding jobs.
+ * This Module is used for linking the service to the router.
+ * acts as the 'controller'
  */
 
+const jobService = require("../services/jobService");
+
 const getAllJobs = (req, res) => {
+    const allJobs = jobService.getAllJobs();
     res.send("Get all jobs");
 };
 
 const getOneJob = (req, res) => {
+    const job = jobService.getOneJob();
     res.send("Get an existing job");
 };
 
 const createCustomJob = (req, res) => {
+    const createJob = jobService.createCustomJob();
     res.send("Create custom job to add to your dashboard");
 };
 
 const updateCustomJob = (req, res) => {
+    const updateJob = jobService.updateCustomJob();
     res.send("Updating a custom job in your dashboard");
 };
 
 const deleteCustomJob = (req, res) => {
+    const deleteJob = jobService.deleteCustomJob();
     res.send("Delete a custome job in your dashboard");
 };
 
---
+++ src/services/jobService.js
@@ -0,0 +1,32 @@
+/**
+ * This Module is used for the actual logic
+ * of the functions used for each API call.
+ */
+
+const getAllJobs = () => {
+    return;
+};
+
+const getOneJob = () => {
+    return;
+};
+
+const createCustomJob = () => {
+    return;
+};
+
+const updateCustomJob = () => {
+    return;
+};
+
+const deleteCustomJob = () => {
+    return;
+};
+
+module.exports = {
+    getAllJobs,
+    getOneJob,
+    createCustomJob,
+    updateCustomJob,
+    deleteCustomJob,
+};
